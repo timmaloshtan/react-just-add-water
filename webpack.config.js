2725dd45b7180d4ce07fe77a446c2be97b587528
Tim M <tim917@gmail.com>
2019-05-20 07:54:54 +0300

    Dev server config

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,35 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'bundle.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /(node_modules)/,
-        use: {
-          loader: 'babel-loader',
+module.exports = function (env) {
+  return {
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'build'),
+      filename: 'bundle.js',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /(node_modules)/,
+          use: {
+            loader: 'babel-loader',
+          },
         },
-      },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        filename: 'index.html',
+      }),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './index.html',
-      filename: 'index.html',
-    }),
-  ],
-};
+    devServer: {
+      contentBase: path.join(__dirname, 'build'),
+      port: 8080,
+      open: 'Google Chrome', // 'google-chrome' on Linux and 'chrome' on Windows
+    },
+    devtool: env.development ? 'source-map' : '',
+  };
+}
